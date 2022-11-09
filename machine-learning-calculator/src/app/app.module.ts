e6f7dd94a31a6716459710af13d61677b65417d1
NeroUcH <hin1997101@gmail.com>
2022-11-10 01:13:23 +0800

    feat: init angular project and import ng-zorro

---
+++ machine-learning-calculator/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_TW } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(zh);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    { provide: NZ_I18N, useValue: zh_TW }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
